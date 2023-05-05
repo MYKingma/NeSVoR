5c3752e43de8fb2f1d8de156ba2fd955cea2712a
MYKingma <55245466+MYKingma@users.noreply.github.com>
2023-05-05 13:16:07 +0200

    chore: improve subsample

--- subsample-data.py
+++ subsample-data.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 import nibabel as nib
 import sys
-from skimage.measure import block_reduce
+from skimage.measure import block_reduce, label
 
 # Plot volume
 
@@ -36,7 +36,7 @@ class IndexTracker(object):
         self.im.axes.figure.canvas.draw()
 
 
-def plotVolume(volumeData):
+def plot_volume(volumeData):
     fig, ax = plt.subplots(1, 1)
     plt.gray()
 
@@ -46,7 +46,7 @@ def plotVolume(volumeData):
     plt.show()
 
 
-def plotSliceInOrientationFromVolume(volumeData, orientation, sliceNumber):
+def plot_slice_in_orientation_from_volume(volumeData, orientation, sliceNumber):
     if orientation == 0:
         sliceData = volumeData[sliceNumber, :, :]
     elif orientation == 1:
@@ -61,18 +61,18 @@ def plotSliceInOrientationFromVolume(volumeData, orientation, sliceNumber):
     plt.show()
 
 
-def getFirstNiftiFileInDir(path):
+def get_first_nifti_file_in_dir(path):
     niftiFiles = [f for f in os.listdir(path) if f.endswith('.nii.gz')]
     return niftiFiles[0]
 
 
-def subsampleSliceData(sliceData, subsampleRate):
+def subsample_slice_data(sliceData, subsampleRate):
     # Subsample the slice data
     subsampledSliceData = sliceData[::subsampleRate, ::subsampleRate]
     return subsampledSliceData
 
 
-def getSliceFromDimension(volumeData, dimension, sliceNumber):
+def get_slice_from_dimension(volumeData, dimension, sliceNumber):
     # Get the slice from the volume data
     if dimension == 0:
         sliceData = volumeData[sliceNumber, :, :]
@@ -86,49 +86,6 @@ def getSliceFromDimension(volumeData, dimension, sliceNumber):
     return sliceData
 
 
-def subsampleVolumeForSliceDimension(volumeData, dimension, subsampleRate):
-    sliceStack = []
-    for sliceNumber in range(volumeData.shape[dimension]):
-        sliceData = getSliceFromDimension(volumeData, dimension, sliceNumber)
-        subsampledSliceData = subsampleSliceData(sliceData, subsampleRate)
-        sliceStack.append(subsampledSliceData)
-    subsampledVolumeData = np.stack(sliceStack, axis=dimension)
-    return subsampledVolumeData
-
-
-# def subsample_volume(volumeData, subsampleRate):
-#     # Subsample the volume data
-#     subsampledVolumeData = volumeData[::subsampleRate,
-#                                       ::subsampleRate, ::subsampleRate]
-#     return subsampledVolumeData
-
-# def subsample_volume(volumeData, subsampleRate):
-#     # Get dimensions of the input volume
-#     xDim, yDim, zDim = volumeData.shape
-
-#     # Compute the new dimensions of the subsampled volume
-#     xNewDim = int(np.ceil(xDim / subsampleRate))
-#     yNewDim = int(np.ceil(yDim / subsampleRate))
-#     zNewDim = int(np.ceil(zDim / subsampleRate))
-
-#     # Create an empty array to store the subsampled volume data
-#     subsampledVolumeData = np.zeros((xNewDim, yNewDim, zNewDim))
-
-#     # Iterate over each subsampled pixel and compute the average value of the corresponding pixels in the input volume
-#     for xIndex in range(xNewDim):
-#         for yIndex in range(yNewDim):
-#             for zIndex in range(zNewDim):
-#                 xStart = xIndex * subsampleRate
-#                 xEnd = min(xStart + subsampleRate, xDim)
-#                 yStart = yIndex * subsampleRate
-#                 yEnd = min(yStart + subsampleRate, yDim)
-#                 zStart = zIndex * subsampleRate
-#                 zEnd = min(zStart + subsampleRate, zDim)
-#                 subsampledVolumeData[xIndex, yIndex, zIndex] = np.mean(
-#                     volumeData[xStart:xEnd, yStart:yEnd, zStart:zEnd])
-
-#     return subsampledVolumeData
-
 def subsample_volume(volume, subsample_rate):
     # Define the downsampling factor based on the subsample rate
     factor = int(subsample_rate)
@@ -141,7 +98,7 @@ def subsample_volume(volume, subsample_rate):
     return downsampled_volume
 
 
-def saveStackInDirectory(subsampledVolumeData, orientation, niftiiFilename, outputPath):
+def save_stack_in_directory(subsampledVolumeData, orientation, niftiiFilename, outputPath):
     newFilename = niftiiFilename + "_" + str(orientation) + ".nii.gz"
     patientId = niftiiFilename.split("_")[0]
     newNifti = nib.Nifti1Image(subsampledVolumeData, np.eye(4))
@@ -155,9 +112,10 @@ def saveStackInDirectory(subsampledVolumeData, orientation, niftiiFilename, outp
     nib.save(newNifti, os.path.join(path, newFilename))
 
 
-def compute_brain_mask(volume, threshold=None, min_size=1000):
+def compute_brain_mask(volume, threshold=None, min_size=100):
     # Compute threshold value using Otsu's algorithm if not provided
     if threshold is None:
+        print(threshold_otsu(volume))
         threshold = threshold_otsu(volume)
 
     # Apply threshold to the volume
@@ -173,72 +131,92 @@ def compute_brain_mask(volume, threshold=None, min_size=1000):
         mask[i, :, :] = binary_fill_holes(mask[i, :, :])
 
     # Remove any small disconnected regions
+    mask = mask > 0
+    # mask = remove_small_objects(label(mask), min_size=min_size)
     mask = remove_small_objects(mask, min_size=min_size)
-
     mask = np.where(mask == True, 1.0, 0.0)
 
-
     return mask
+
+
+def normalize_volume(volume):
+    # Normalize the volume to be between 0 and 1
+    volume = volume - np.min(volume)
+    volume = volume / np.max(volume)
+
+    return volume
+
+
+def load_nifti_file(niftiPath):
+    # Open the nifti file
+    nifti = nib.load(niftiPath)
+
+    # Get the data from the nifti file
+    nifti_data = nifti.get_fdata()
+
+    # Get absolute value of data
+    nifti_data = np.abs(nifti_data)
+
+    return nifti_data
+
+
+def preprocess_file(nifti_path, nifti_file_name, output_path):
+    # Load the nifti file
+    nifti_data = load_nifti_file(nifti_path)
+
+    # Normalize the volume
+    nifti_data = normalize_volume(nifti_data)
+
+    # Subsample the volume data
+    subsampledVolumeDataX = subsample_volume(nifti_data, 2)
+    subsampledVolumeDataY = subsample_volume(nifti_data, 2)
+    subsampledVolumeDataZ = subsample_volume(nifti_data, 2)
+
+    # Save the subsampled volume data
+    save_stack_in_directory(subsampledVolumeDataX, 0,
+                            nifti_file_name, output_path)
+    save_stack_in_directory(subsampledVolumeDataY, 1,
+                            nifti_file_name, output_path)
+    save_stack_in_directory(subsampledVolumeDataZ, 2,
+                            nifti_file_name, output_path)
+
+    # Compute the brain mask of first orientation
+    brainMask = compute_brain_mask(subsampledVolumeDataX, 0.085)
+
+    # Save the brain mask
+    save_stack_in_directory(
+        brainMask, 0, nifti_file_name + "_mask", output_path)
 
 
 def main():
     # Get the path to the directory containing the nifti files
     path = sys.argv[1]
-    outputPath = sys.argv[2]
+    output_path = sys.argv[2]
 
-    # Get the first nifti file in the directory
-    niftiFile = getFirstNiftiFileInDir(path)
+    # Get parameter which defines if all or one file should be processed
+    process_all_files = True
+    if len(sys.argv) > 3:
+        process_all_files = False
 
-    # Open the nifti file
-    niftiPath = os.path.join(path, niftiFile)
-    nifti = nib.load(niftiPath)
+    if process_all_files:
+        # Loop over files in directory
+        for file_name in os.listdir(path):
 
-    # Get the data from the nifti file
-    niftiData = nifti.get_fdata()
+            # Ignore if file is ds_store
+            if file_name == ".DS_Store":
+                continue
 
-    # Get absolute value of data
-    niftiData = np.abs(niftiData)
+            # Get nifti filepath
+            nifti_file_path = os.path.join(path, file_name)
 
-    # Normalize values in volume between 0 and 1
-    niftiData = niftiData / np.max(np.abs(niftiData))
-    
-    # # Plot histogram of values in volume
-    # plt.hist(niftiData.flatten(), bins=100)
-    # plt.show()
+            # Preprocess the file
+            preprocess_file(nifti_file_path, file_name, output_path)
+    else:
+        # Get first nifti file in directory
+        nifti_file_path = get_first_nifti_file_in_dir(path)
 
-    # # Plot histogram of values in volume
-    # plt.hist(niftiData.flatten(), bins=100)
-    # plt.show()
-
-    # # Subsample the volume data
-    # subsampledVolumeDataX = subsampleVolumeForSliceDimension(niftiData, 0, 2)
-    # subsampledVolumeDataY = subsampleVolumeForSliceDimension(niftiData, 1, 2)
-    # subsampledVolumeDataZ = subsampleVolumeForSliceDimension(niftiData, 2, 2)
-
-    # # Subsample the volume data
-    subsampledVolumeDataX = subsample_volume(niftiData, 2)
-    subsampledVolumeDataY = subsample_volume(niftiData, 2)
-    subsampledVolumeDataZ = subsample_volume(niftiData, 2)
-
-    # # Save the subsampled volume data
-    saveStackInDirectory(subsampledVolumeDataX, 0, niftiFile, outputPath)
-    saveStackInDirectory(subsampledVolumeDataY, 1, niftiFile, outputPath)
-    saveStackInDirectory(subsampledVolumeDataZ, 2, niftiFile, outputPath)
-
-    # Compute the brain mask of first orientation
-    brainMask = compute_brain_mask(subsampledVolumeDataX, 0.1)
-
-    # brainMask = np.where(subsampledVolumeDataX > 0, subsampledVolumeDataX, 0.0)
-    # brainMask = np.where(brainMask > 0, 1, 0)
-
-    # # plot mask and volume side by side
-    # fig, axes = plt.subplots(1, 2)
-    # axes[0].imshow(subsampledVolumeDataX[15, :, :], cmap="gray")
-    # axes[1].imshow(brainMask[15, :, :], cmap="gray")
-    # plt.show()
-
-    # # Save the brain mask
-    # saveStackInDirectory(brainMask, 0, niftiFile + "_mask", outputPath)
+        # Preprocess the file
+        preprocess_file(nifti_file_path, output_path, output_path)
 
 
 if __name__ == "__main__":
